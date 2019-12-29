e152ed65f482ebddbca10205417af3b55f21d08f
Jakub Tomas <tomasjakub21@gmail.com>
2019-12-29 15:20:41 +0100

    add BufferedRead add data from students.dat

--- src/goodSchool/project1/Main.java
+++ src/goodSchool/project1/Main.java
@@ -1,55 +1,81 @@
 package goodSchool.project1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Arrays;
 
 public class Main {
 
-
+    private static School school;
 
     public static void main(String[] args) {
 
-
-        System.out.println("helloussss world");
-
-        School school1 = new School();
-        Student student1 = new Student("Jednotka", "aSagan", (byte) 1, (byte) 1, (byte) 4, 312);
-        Student student2 = new Student("trojka", "priezvisko", (byte) 4, (byte) 2, (byte) 2, 90);
-        Student student3 = new Student("dvojka", "prezvisko", (byte) 1, (byte) 1, (byte) 1, 133);
-        Student student4 = new Student("stvorka", "mrkvicka", (byte) 1, (byte) 2, (byte) 1, 12);
-        Student student5 = new Student("petka", "Petrojovic", (byte) 2, (byte) 2, (byte) 1, 5);
-        Student student6 = new Student("sestka", "sestakova", (byte) 2, (byte) 2, (byte) 1, 4);
-        Student student7 = new Student("sedem", "sestakova", (byte) 2, (byte) 2, (byte) 1, 3);
-        Student student8 = new Student("osmicka", "sestakova", (byte) 2, (byte) 2, (byte) 1, 1);
-
-        System.out.println(student1.getG_Eng());
-        System.out.println(student1.getAverageGrade());
-
-        school1.addStudent(student2);
-        school1.addStudent(student6);
-        school1.addStudent(student4);
-        school1.addStudent(student5);
-        school1.addStudent(student1);
-        school1.addStudent(student7);
-        school1.addStudent(student8);
-
-        school1.addStudent(student3);
-
-        System.out.println("Average absence all students is " + school1.getAverageAbsence());
-
-        System.out.println("The Lowest Absence is           " +  school1.getTheLowestAbsence());
-
-        System.out.println("Average grade all students is "   + school1.getAverageGrade());
-
-        System.out.println(" Average grade from Math all students is " + school1.getAverageGradeFromMath());
-
-        System.out.println("The best student in this amazing school is "+ school1.getTheBestStudent().getFullName());
-
-        System.out.println(" The worst student in this amazing shchool is " + school1.getTheWorstStudent().getFullName());
+        Student student;
+        School school = new School();
+        String data;
 
 
-        System.out.println(Arrays.toString(school1.getListSortedByAbsenceDesc().getItems()));
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("students.dat"));
+            while((data = br.readLine()) != null) {
+                String[] arrOfStr = data.split(" ");
+                String firstName = arrOfStr[0];
+                String lastName = arrOfStr[1];
+                byte gmat = Byte.valueOf(arrOfStr[2]);
+                byte geng = Byte.valueOf(arrOfStr[3]);
+                byte ginf = Byte.valueOf(arrOfStr[4]);
+                int absence = Integer.valueOf(arrOfStr[5]);
 
-        System.out.println(Arrays.toString(school1.getListSortedByLastNameAsc().getItems()));
+                student = new Student(firstName,lastName,gmat,geng,ginf,absence);
+
+                school.addStudent(student);
+                System.out.println(student.getFullName()
+                        + " "
+                        + student.getG_Mat()
+                        + " "
+                        + student.getG_Eng()
+                        + " " + student.getG_Inf()
+                        + " " + student.getAbsence());
+            }
+            br.close();
+            System.out.println(School.getCount());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Average absence all students is " + school.getAverageAbsence());
+
+        System.out.println("The Lowest Absence is           " +  school.getTheLowestAbsence());
+
+
+        System.out.println("Average grade all students is   " + school.getAverageGrade());
+
+
+        System.out.println(" Average grade  from MATH       " + school.getAverageGradeFromMath());
+
+
+        System.out.println("The best student  is             "+ school.getTheBestStudent().getFullName());
+
+
+        System.out.println(" The worst student  is          " + school.getTheWorstStudent().getFullName());
+
+        System.out.println("");
+
+        System.out.println("sort by absence desc");
+        System.out.println(Arrays.toString(school.getListSortedByAbsenceDesc().getItems()));
+        System.out.println("");
+
+        System.out.println("sort by the LastName");
+        System.out.println(Arrays.toString(school.getListSortedByLastNameAsc().getItems()));
 
 }
 }
